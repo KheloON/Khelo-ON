1b4822db37994bdb847648eb0808d3f646b3a681
Mayank Singh Tomar <143412700+mayank-singh-tomar@users.noreply.github.com>
2025-03-18 16:02:16 +0530

    Add files via upload

---
+++ KheloON/App/android/app/src/main/kotlin/com/example/athlete/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.company.athleticon
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterFragmentActivity() {
+    
+}
