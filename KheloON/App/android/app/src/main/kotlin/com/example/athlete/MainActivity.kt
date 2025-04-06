446175dad0261b0416bcc1237485c4a1fae45527
Mayank Singh Tomar <143412700+mayank-singh-tomar@users.noreply.github.com>
2025-04-06 12:02:46 +0530

    Remove all cached files to fix .gitignore issues

--- KheloON/App/android/app/src/main/kotlin/com/example/athlete/MainActivity.kt
+++
@@ -1,9 +0,0 @@
-package com.company.KheloON
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterFragmentActivity() {
-    
-}
